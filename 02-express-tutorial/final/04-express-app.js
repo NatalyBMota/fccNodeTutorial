beb44d1be9c79dbe613666936609d87bf197910f
NatalyBMota <interactivedesign@yahoo.com>
2025-12-16 19:41:33 -0500

    first commit

---
+++ 02-express-tutorial/final/04-express-app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+// get all files
+
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './navbar-app/index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile('About page')
+})
+
+/*
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+*/
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....')
+})
